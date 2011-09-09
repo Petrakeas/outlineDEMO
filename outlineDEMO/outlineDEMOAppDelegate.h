81554764f390fd7c862c0b74eb3697fcb128672c
Petros Douvantzis <petrakeas@macbook.local>
2011-09-09 23:22:24 +0300

    Initial Commit

---
+++ outlineDEMO/outlineDEMOAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  outlineDEMOAppDelegate.h
+//  outlineDEMO
+//
+//  Created by Petros Douvantzis on 9/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface outlineDEMOAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
